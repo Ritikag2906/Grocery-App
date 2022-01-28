3fc0f98468b4b8f11071a55ec9d75fdc40366f45
Ritika <theritikagupta29@gmail.com>
2022-01-28 11:48:16 +0530

    .

--- lib/services/db_service.dart
+++ lib/services/db_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:grocery_app/Models/grocery_item.dart';
 
 class DatabaseService{
@@ -15,5 +16,16 @@ class DatabaseService{
         .map((event) => event.docs
             .map((e) => Item.fromMap(e.data()! as Map<String, dynamic>))
             .toList());
+  }
+
+  Future<void> addGroceryItem(Item item) async {
+
+    // Reference to the path in firestore where the collection reside.
+    final CollectionReference ref =
+        FirebaseFirestore.instance.collection('grocery');
+    // adding to database
+    await ref.add(item.toMap()).catchError((error) {
+      debugPrint('Error Occurred: $error');
+    });
   }
 }
